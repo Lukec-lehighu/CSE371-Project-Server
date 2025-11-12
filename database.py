f88fed45c22d7e6408a6b5fc800a5a68143ead0c
Luke Crimi <lukecrimi15@gmail.com>
2025-11-12 10:30:21 -0500

    Auth working with getting and making groups

--- database.py
+++ database.py
@@ -2,29 +2,52 @@ import sqlite3
 
 DB_NAME = 'groceries.db'
 MAX_GROUPNAME_LEN = 50
-MAX_OWNERNAME_LEN = 50
+MAX_USERNAME_LEN = 50
+
+def _table_exists(name, cur):
+    res = cur.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{name}'")
+    return not res.fetchone() is None
 
 def setupTables():
     cur = sqlite3.connect(DB_NAME).cursor()
-    res = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='groups'")
-    if res.fetchone() is None:
+    if not _table_exists('groups', cur):
         cur.execute(f"""CREATE TABLE groups(
                         groupname varchar({MAX_GROUPNAME_LEN}) PRIMARY KEY,
-                        owner varchar({MAX_GROUPNAME_LEN}),
+                        owner varchar({MAX_USERNAME_LEN}),
                         public int
                     )
                     """)
         print('Created table: groups')
     
-    res = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='receipts'")
-    if res.fetchone() is None:
-        cur.execute("CREATE TABLE receipts(rnum, groupname, owner, items, prices, members paid)")
+    if not _table_exists('receipt_data', cur):
+        cur.execute(f"""CREATE TABLE receipt_data(
+                        rnum PRIMARY KEY,
+                        groupname,
+                        owner varchar({MAX_USERNAME_LEN})
+                    )""")
         print('Created table: receipts')
 
-def getGroups():
+    if not _table_exists('group_members', cur):
+        cur.execute(f"""CREATE TABLE group_members(
+                        groupname varchar({MAX_GROUPNAME_LEN}),
+                        membername varchar({MAX_USERNAME_LEN})
+                    )""")
+        print('Created table: group_members')
+
+def getGroups(username):
+    # get all of the groups that the user has access to 
     cur = sqlite3.connect(DB_NAME).cursor()
-    res = cur.execute("SELECT * FROM groups WHERE public=1")
-    return res.fetchall()
+    pubRes = cur.execute("SELECT groupname FROM groups WHERE public=1").fetchall()
+    privRes = cur.execute(f"SELECT groupname FROM group_members WHERE membername='{username}'").fetchall()
+
+    if pubRes is None:
+        pubRes = []
+    if privRes is None:
+        privRes = []
+
+    pubRes.extend(privRes)
+    res = list(set(pubRes)) # get rid of duplicates
+    return res
 
 def newGroup(groupname, ownername, public):
     con = sqlite3.connect(DB_NAME)
@@ -32,13 +55,16 @@ def newGroup(groupname, ownername, public):
     # Trim names if necessary
     if len(groupname) > MAX_GROUPNAME_LEN:
         groupname = groupname[0:MAX_GROUPNAME_LEN]
-    if len(ownername) > MAX_OWNERNAME_LEN:
-        ownername = ownername[0:MAX_OWNERNAME_LEN]
+    if len(ownername) > MAX_USERNAME_LEN:
+        ownername = ownername[0:MAX_USERNAME_LEN]
 
     # TODO: have more checks to avoid SQL code injections and such
+
     try:
         cur.execute("INSERT INTO groups (groupname, owner, public) VALUES" \
                     f"('{groupname}', '{ownername}', {1 if public else 0})")
+        cur.execute("INSERT INTO group_members (groupname, membername) VALUES" \
+                    f"('{groupname}', '{ownername}')")
         con.commit()
         return True
     except Exception as e:
--- main.py
+++ main.py
@@ -1,23 +1,64 @@
 from flask import Flask, jsonify, request
-from database import getGroups
+from flask_cors import CORS
+from database import getGroups, newGroup
+
+import requests
+import json
 
 DEVRUN = True # whether or not I'm running the script for development or testing
 
 app = Flask(__name__)
+CORS(app)
+
+# Helper function to check if the user has authority to use an email as their username
+def check_auth(token):
+    res = requests.get(f'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={token}')
+    response = json.loads(res.text)
+    return response.get('email', '')
 
 # get the names of all the groups the user has access to
-@app.route('/groups', methods=['GET'])
+@app.route('/groups', methods=['POST'])
 def get_groups():
-    groups = getGroups()
-    if groups is None:
-        groups = []
-    return jsonify(groups)
+    resp = {}
+
+    body = request.json
+    if body:
+        token = body.get('token', '')
+
+        username = check_auth(token)
+        if len(username) == 0:
+            resp['error'] = 'User is not signed in!'
+        else:
+            groups = getGroups(username=username)
+            resp['ok'] = groups
+    else:
+        resp['error'] = 'INVALID REQUEST'
+
+    return resp
 
 @app.route('/make_group', methods=['POST'])
 def make_group():
     resp = {}
 
-    print(request.json)
+    body = request.json
+    if body:
+        name = body.get('name', '')
+        owner = body.get('owner', '') # this will be an auth token, NOT an email
+        public = body.get('public', 1) # public by default if the user doesn't specify (maybe handle this with error but for now this will do)
+
+        ownerEmail = check_auth(owner) # convert auth token to email
+
+        if len(name)==0:
+            resp['error'] = "Group or owner name not valid!"
+        elif len(ownerEmail)==0:
+            resp['error'] = "Not signed in! Please refresh the page."
+        else:
+            if newGroup(name, ownerEmail, public):
+                resp['ok'] = 'Group created'
+            else:
+                resp['error'] = f'Group name "{name}" already exists in database!'
+    else:
+        resp['error'] = 'INVALID REQUEST'
 
     return jsonify(resp)
 
